ff40614765ae67b09653c92d46745857bca8710d
Felix <fguzmanceri@gmail.com>
2023-01-21 18:50:57 -0400

    changes

--- src/js/product.js
+++ src/js/product.js
@@ -1,46 +1,51 @@
-import { getParam } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage, getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-import ProductDetails from "./ProductDetails.mjs";
 
-const productId = getParam("product");
 const dataSource = new ProductData("tents");
+const productId = new getParam("product");
+console.log(productId);
 
-const product = new ProductDetails(productId, dataSource);
-product.init();
+function addProductToCart(product) {
+  let cart = getLocalStorage("soCart");
+  if (cart) {
+    let tent = 1;
+    for (let i = 0; i < cart.length; i++) {
+      if (cart[i].Id == product.Id) {
+        product.quantity = cart[i].quantity++;
+        tent = 0;
+      }
+    }
 
-//console.log(dataSource.findProductById(productId));
+    if (tent == 1) {
+      product.quantity = 1;
+      cart.push(product);
+    }
+  } else {
+    cart = [];
+    product.quantity = 1;
+    cart.push(product);
+  }
 
-// function addProductToCart(product) {
-//   let cart = getLocalStorage("so-cart");
-//   if (cart) {
-//     let tent = 1;
-//     for (let i = 0; i < cart.length; i++) {
-//       if (cart[i].Id == product.Id) {
-//         product.quantity = cart[i].quantity++;
-//         tent = 0;
-//       }
-//     }
+  setLocalStorage("soCart", cart);
+}
 
-//     if (tent == 1) {
-//       product.quantity = 1;
-//       cart.push(product);
-//     }
-//   } else {
-//     cart = [];
-//     product.quantity = 1;
-//     cart.push(product);
-//   }
+// add to cart button event handler
+async function addToCartHandler(e) {
+  const product = await dataSource.findProductById(e.target.dataset.id);
+  addProductToCart(product);
+  // Get the button element by its ID
+var button = document.getElementById("addToCart");
 
-//   setLocalStorage("so-cart", cart);
-// }
+// Add an event listener to the button
+button.addEventListener("click", function() {
+    // This function will be called when the button is clicked
+    alert("You purchase!" + "" + product["Name"])
+    
+  });
+  
+}
 
-// // add to cart button event handler
-// async function addToCartHandler(e) {
-//   const product = await dataSource.findProductById(e.target.dataset.id);
-//   addProductToCart(product);
-// }
-
-// // add listener to Add to Cart button
-// document
-//   .getElementById("addToCart")
-//   .addEventListener("click", addToCartHandler);
+// add listener to Add to Cart button
+document
+  .getElementById("addToCart")
+  .addEventListener("click", addToCartHandler);
