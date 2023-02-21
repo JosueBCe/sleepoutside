669482573c93b407f52c646d426e239a20cde245
Felix <fguzmanceri@gmail.com>
2023-02-20 22:59:02 -0400

    Looking some js

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -9,7 +9,6 @@ myCheckout.init()
 document.querySelector("#zip").addEventListener("blur", myCheckout.calculateOrdertotal.bind(myCheckout));
 document.querySelector("#checkoutSubmit").addEventListener("click", (e) => {
     e.preventDefault();
-
+    
     myCheckout.checkout();
 });
-
