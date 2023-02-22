3f482f16873fccd9a019263eb7613d248b25846a
Felix Samuel Guzman Ceri <113263790+FelixS2000@users.noreply.github.com>
2023-02-22 09:25:09 -0400

    Update product-listing.js

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -16,16 +16,3 @@ const listing = new ProductListing(category, dataSource, element);
 logProductCard();
 
 listing.init();
-
-// Create the breadcrumb navigation
-const breadcrumbElement = document.querySelector(".breadcrumb");
-let breadcrumbHtml = "";
-if (category) {
-  breadcrumbHtml += `<a href="/">Home</a> > <a href="/?category=${category}">${category}</a>`;
-  if (product) {
-    breadcrumbHtml += ` > ${product}`;
-  }
-} else {
-  breadcrumbHtml += "Home";
-}
-breadcrumbElement.innerHTML = breadcrumbHtml;
