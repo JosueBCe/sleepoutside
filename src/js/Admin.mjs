6780b0e7394264e091a747a1e45e78cf6fd75c48
Felix <fguzmanceri@gmail.com>
2023-02-28 08:02:24 -0400

    saving

---
+++ src/js/Admin.mjs
@@ -0,0 +1,20 @@
+const baseURL = "http://server-nodejs.cit.byui.edu:3000/login";
+//constructor(outputSelector) {
+//  this.mainElement = document.querySelector(outputSelector);
+//  this.token = null;
+//  this.services = new ExternalServices();
+//}
+//async login(creds, next) {
+// I built the login method with a callback: next. 
+// This makes it much more flexible...
+// there could be many different things the user wants to do after logging in...
+// this allows us that flexibility without having to write a bunch of login methods
+//try {
+//  this.token = await this.services.loginRequest(creds);
+//  next()
+//} 
+//catch(err) {
+  // remember this from before?
+//  alertMessage(err.message.message);
+//}
+//}
