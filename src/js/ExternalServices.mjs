a13f3ae4b2a40f92918038fa411fd37e5f104ff4
Felix <fguzmanceri@gmail.com>
2023-03-01 11:52:05 -0400

    saving

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -1,59 +1,67 @@
-//const baseURL = "http://server-nodejs.cit.byui.edu:3000/";
- const baseURL = "https://wdd330-backend.onrender.com/";
-
-function convertToJson(res) {
+// const baseURL = "http://server-nodejs.cit.byui.edu:3000/";
+const baseURL = "https://wdd330-backend.vercel.app/";
+// const baseURL = "http://localhost:3000/";
+async function convertToJson(res) {
+  const data = await res.json();
   if (res.ok) {
-    return res.json();
+    return data;
   } else {
-    // throw new Error("Bad Response"); const jsonResponse = await res.json();
-    throw { name: "servicesError", message: res.json() };
+    throw { name: "servicesError", message: data };
   }
 }
 
 export default class ExternalServices {
-   constructor(category) {
-  //   // this.category = category;
-  //   // this.path = `../json/${this.category}.json`;
-   }
-
+  constructor(category) {
+    // this.category = category;
+    // this.path = `../json/${this.category}.json`;
+  }
   async getData(category) {
-
-    // http://server-nodejs.cit.byui.edu:3000/products/search/tents
-
     const response = await fetch(baseURL + `products/search/${category}`);
     const data = await convertToJson(response);
     return data.Result;
-  
   }
-  // getData() {
-  //   return fetch(this.path)
-  //     .then(convertToJson)
-  //     .then((data) => data);
-  // }
-  
   async findProductById(id) {
-
-    // http://server-nodejs.cit.byui.edu:3000/product/989CH
-    
     const response = await fetch(baseURL + `product/${id}`);
     const data = await convertToJson(response);
     return data.Result;
-
-    // const products = await this.getData();
-    // return products.find((item) => item.Id === id);
   }
-
   async checkout(payload) {
     const options = {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(payload)
-    }
-    // const response = await fetch(baseURL + "checkout/", options);
-    // const data = await convertToJson(response);
-    // return data.Result;
+      body: JSON.stringify(payload),
+    };
     return await fetch(baseURL + "checkout/", options).then(convertToJson);
+  }
+  async loginRequest(user) {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    };
+    const response = await fetch(baseURL + "login", options).then(
+      convertToJson
+    );
+    return response.accessToken;
+  }
+  // make a request to the server for the current orders
+  // requires: a valid token
+  // returns: a list of orders
+  async getOrders(token) {
+    const options = {
+      method: "GET",
+      // the server will reject our request if we don't include the Authorization header with a valid token!
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await fetch(baseURL + "orders", options).then(
+      convertToJson
+    );
+    return response;
   }
 }
