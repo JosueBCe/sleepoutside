e2d9bf774d58145db158d27806013304f4ca7616
Felix <fguzmanceri@gmail.com>
2023-03-01 11:46:34 -0400

    saving

--- src/js/Admin.mjs
+++ src/js/Admin.mjs
@@ -1,4 +1,4 @@
-class Admin {
+export default class Admin {
   async login(email, password) {
     const response = await fetch("http://server-nodejs.cit.byui.edu:3000/login", {
       method: "POST",
@@ -7,52 +7,46 @@ class Admin {
       },
       body: JSON.stringify({ email, password })
     });
-
-    if (!response.ok) {
-      throw new Error("Login failed");
+  
+    if (response.status === 401) {
+      throw new Error("Invalid credentials");
     }
-
+  
+    if (!response.ok) {
+      throw new Error("Something went wrong");
+    }
+  
     const data = await response.json();
-
+  
     return {
       email: data.email,
       password: data.password,
       isAdmin: true
     }
   }
-
-  showLogin() {
-    const mainElement = document.querySelector("main");
-    const form = document.createElement("form");
-
-    const emailInput = document.createElement("input");
-    emailInput.type = "email";
-    emailInput.placeholder = "Email";
-    form.appendChild(emailInput);
-
-    const passwordInput = document.createElement("input");
-    passwordInput.type = "password";
-    passwordInput.placeholder = "Password";
-    form.appendChild(passwordInput);
-
-    const submitButton = document.createElement("button");
-    submitButton.textContent = "Submit";
-    submitButton.addEventListener("click", async (event) => {
-      event.preventDefault();
-      const email = emailInput.value;
-      const password = passwordInput.value;
-
-      try {
-        const user = await this.login(email, password);
-        console.log(user);
-      } catch (error) {
-        console.error(error);
-      }
+  async showLogin(email,password){
+    const response = await fetch("http://server-nodejs.cit.byui.edu:3000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ email, password })
     });
-    form.appendChild(submitButton);
-
-    mainElement.appendChild(form);
+  
+    if (response.status === 401) {
+      throw new Error("Invalid credentials");
+    }
+  
+    if (!response.ok) {
+      throw new Error("Something went wrong");
+    }
+  
+    const data = await response.json();
+  
+    return {
+      email: data.email,
+      password: data.password,
+      isAdmin: true
+    }
   }
-}
-
-export default Admin;
+};
