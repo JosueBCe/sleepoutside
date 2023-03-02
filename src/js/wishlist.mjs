c3683412f73863de118cd98e85f7a1c27af9c70e
Felix <fguzmanceri@gmail.com>
2023-03-02 19:24:16 -0400

    saving

--- src/js/wishlist.mjs
+++ src/js/wishlist.mjs
@@ -1,70 +1,44 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getLocalStorage } from "./utils.mjs";
 import { sumTotal } from "./ShoppingCart.mjs";
-import addToCart from "./ProductDetails.mjs";
 
-export default class ShoppingWishCart {
+export default class ShoppingWishCart{
   constructor(key, parentSelector) {
     this.key = key;
     this.parentSelector = parentSelector;
   }
 
-  renderWishlistContents() {
+  renderWishlistContents(){
     const wishlistItems = getLocalStorage(this.key) || [];
     let wishlistTotal = document.querySelector(".wishlist-total");
-    if (wishlistTotal !== 0) {
+    if (wishlistTotal !== 0) { // Check if wishlistTotal is not null
       if (wishlistItems.length != 0) {
-        const htmlItems = wishlistItems.map((item) =>
-          wishlistItemTemplate(item)
-        );
-        document.querySelector(this.parentSelector).innerHTML = htmlItems.join(
-          ""
-        );
-        if (wishlistTotal !== null) {
-          wishlistTotal.style.display = "block";
-          wishlistTotal.innerHTML = `Total: $${sumTotal(wishlistItems).toFixed(
-            2
-          )}`;
-
-          const wishlistButtons = document.querySelectorAll(".wishlist-button");
-          wishlistButtons.forEach((button) => {
-            button.addEventListener("click", () => {
-              const itemId = button.dataset.id;
-              addToCart(itemId);
-              this.removeFromWishlist(itemId);
-              const updatedWishlistItems = getLocalStorage(this.key);
-              this.renderWishlistContents(updatedWishlistItems);
-            });
-          });
+        const htmlItems = wishlistItems.map((item) => wishlistItemTemplate(item));
+        document.querySelector(this.parentSelector).innerHTML = htmlItems.join("");
+        if (wishlistTotal !== null){
+          wishlistTotal.style.display = "block"; // Make appear the total paragraph that is hidden by default
+        wishlistTotal.innerHTML = `Total: $${sumTotal(wishlistItems).toFixed(2)}`
         }
       }
     }
   }
-
-  removeFromWishlist(itemId) {
-    const wishlistItems = getLocalStorage(this.key) || [];
-    const updatedWishlistItems = wishlistItems.filter(
-      (item) => item.Id !== itemId
-    );
-    setLocalStorage(this.key, updatedWishlistItems);
-    this.renderWishlistContents(updatedWishlistItems);
-  }
 }
 
-function wishlistItemTemplate(product) {
-  let final_price = Number(product.FinalPrice);
-  let suggested_retail_price = Number(product.SuggestedRetailPrice);
+function wishlistItemTemplate(item) {
+  let final_price = Number(item.FinalPrice);
+  let suggested_retail_price = Number(item.SuggestedRetailPrice);
   let discount = Math.abs(final_price - suggested_retail_price).toFixed(2);
-  let quantity = Number(product.quantity);
+  let quantity = Number(item.quantity);
   let total_discount = (discount * quantity).toFixed(2);
-  let { Images, Name } = product;
+  let { Images, Name } = item;
   let total_price = Number(final_price * quantity).toFixed(2);
 
+  // Check if the quantity, total_discount, and total_price are valid numbers before displaying them
   if (isNaN(quantity)) quantity = 0;
   if (isNaN(total_discount)) total_discount = 0;
   if (isNaN(total_price)) total_price = 0;
 
-  const newItem = `<li class='wishlist-cart-card divider'>
-                    <a href='#' class='wishlist-cart-card__image'>
+  const newItem = `<li class="wishlist-cart-card divider">
+                    <a href="#" class="wishlist-cart-card__image">
                     <img
                       src="${Images.PrimaryMedium}"
                       srcset="${Images.PrimarySmall} 350w,
@@ -78,14 +52,12 @@ function wishlistItemTemplate(product) {
                       alt="Image of ${Name}"
                     />
                     </a>
-                    <a href='#'>
-                      <h2 class='card__name'>${product.Name}</h2>
+                    <a href="../product_pages/index.html?product=${item.Id}">
+                      <h2 class="wishlist-card__name">${item.Name}</h2>
+                      <button class="add-to-cart" id="${item.Id}>Add to Cart</button>
                     </a>
-                    <p class='wishlist-cart-card__color'>${
-                      product.Colors[0].ColorName
-                    }</p>
-                    <button class="addToCart" data-id="${product.Id}>Add to Cart</button>
                   </li>
                   `;
   return newItem;
 }
+
